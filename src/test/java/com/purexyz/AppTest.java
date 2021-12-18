c04474c49db8e4e4d5915cb546a1c27c492ff371
PureXYZ <andydai@live.ca>
2021-12-17 22:03:45 -0500

    Update test

--- src/test/java/com/purexyz/AppTest.java
+++ src/test/java/com/purexyz/AppTest.java
@@ -1,19 +1,43 @@
 package com.purexyz;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 class AppTest {
 
+  static InputStream inputStream;
+
+  @BeforeAll
+  static void setUp() {
+    inputStream = System.in;
+  }
+
+  @AfterAll
+  static void tearDown() {
+    System.setIn(inputStream);
+  }
+
+  void setSystemInput(String... inputs) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (String input : inputs) {
+      stringBuilder.append(input);
+      stringBuilder.append(System.lineSeparator());
+    }
+
+    byte[] input = stringBuilder.toString().getBytes(StandardCharsets.UTF_8);
+    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input);
+    System.setIn(byteArrayInputStream);
+  }
+
   @Test
-  void main() throws Exception {
-    AtomicInteger statusCode = new AtomicInteger(1);
-    withTextFromSystemIn("quit")
-        .execute(() -> statusCode.set(catchSystemExit(() -> App.main(null))));
-    assertEquals(0, statusCode.get());
+  void main() {
+    setSystemInput("quit");
+    assertDoesNotThrow(() -> App.main(new String[]{}));
   }
 }
